ee76b09925f3d3016bdcd02492abe782181b2d31
Ross Hoyt <rosshoytmusic@gmail.com>
2021-02-03 23:57:23 -0800

    Added Delete to DatabaseService.cs

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -76,9 +76,26 @@ namespace Mine.Services
             return true;
         }
 
-        public Task<bool> DeleteAsync(string id)
+        /// <summary>
+        /// Deletes an entry form the database asychronously
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteAsync(string id)
         {
-            throw new NotImplementedException();
+            var data = await ReadAsync(id);
+            if(data == null)
+            {
+                return false;
+            }
+
+            var result = await Database.DeleteAsync(data);
+            if(result == 0)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         /// <summary>
