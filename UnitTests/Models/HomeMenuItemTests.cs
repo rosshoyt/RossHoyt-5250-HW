1de804aa7da7f75ed2d7d1930bb329185d8b236e
Ross Hoyt <rosshoytmusic@gmail.com>
2021-02-09 19:31:11 -0800

    Added ItemModel_Get UT to ItemModelTests.cs

---
+++ UnitTests/Models/HomeMenuItemTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mine.Models;
+using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class HomeMenuItemTests
+    {
+        [Test]
+        public void HomeMenuItem_Constructor_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new HomeMenuItem();
+            
+            // Reset 
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void HomeMenuItem_Set_Get_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new HomeMenuItem();
+            result.Id = MenuItemType.Items;
+            result.Title = "Title";
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(MenuItemType.Items, result.Id);
+            Assert.AreEqual("Title", result.Title);
+        }
+    }
+}
